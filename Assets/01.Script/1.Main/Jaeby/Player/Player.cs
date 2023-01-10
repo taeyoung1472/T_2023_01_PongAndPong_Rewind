fbaf4369783207c893569d07aef273e9a6f72dd8
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2023-01-10 20:23:08 +0900

    0110_대시,점프,이동,플레이어

---
+++ Assets/01.Script/1.Main/Jaeby/Player/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerMove _playerMove = null;
+    private PlayerJump _playerJump = null;
+    private PlayerDash _playerDash = null;
+
+    public bool Moveable { get => _playerMove.Moveable; set => _playerMove.Moveable = value; }
+    public bool Jumpable { get => _playerJump.Jumpable; set => _playerJump.Jumpable = value; }
+    public bool Dashable { get => _playerDash.Dashable; set => _playerDash.Dashable = value; }
+
+    private void Awake()
+    {
+        _playerDash = GetComponent<PlayerDash>();
+        _playerJump = GetComponent<PlayerJump>();
+        _playerMove = GetComponent<PlayerMove>();
+    }
+
+    public void PlayerAllActionSet(bool value)
+    {
+        Moveable = Jumpable = Dashable = value;
+    }
+}
