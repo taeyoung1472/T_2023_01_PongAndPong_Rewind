586e6a8e47d6c36a09a6292925eca9f3046a2f53
Taeyoung <l32590838@gmail.com>
2023-01-12 18:39:51 +0900

    순행역행 마무리

--- Assets/01.Script/1.Main/Taeyoung/Rewind/RecordObject.cs
+++ Assets/01.Script/1.Main/Taeyoung/Rewind/RecordObject.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public abstract class RecordObject : MonoBehaviour
 {
     protected float RecordingPercent { get { return RewindManager.Instance.CurRecordingPercent; } }
-    protected int TotalRecordCount { get { return RewindManager.Instance.TotalRecordCount; } }
+    protected int TotalRecordCount 
+    { 
+        get 
+        { 
+            if(totalRecord == -1)
+            {
+                totalRecord = RewindManager.Instance.TotalRecordCount;
+            }
+            return totalRecord;
+        } 
+    }
+    private int totalRecord = -1;
 
     // 본인 흐름의 실행되는 업데이트
     public virtual void OnUpdate() { }
 
-    // 본인 역 흐음때 실행되는 업데이트
+    // 본인 역 흐름때 실행되는 업데이트
     public virtual void OnRewindUpdate() { }
 
     // 본인 흐름으로 바뀔때 실행
