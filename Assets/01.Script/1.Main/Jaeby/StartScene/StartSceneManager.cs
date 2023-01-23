f68cf9fabd240a88f5c9cea383721a404eedf9ec
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2023-01-23 12:45:06 +0900

    0123_스타트씬 하는척

---
+++ Assets/01.Script/1.Main/Jaeby/StartScene/StartSceneManager.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup _logoGroup = null;
+    [SerializeField]
+    private GameObject _mainPanel = null;
+
+    private void Start()
+    {
+        LogoSplash();
+    }
+
+    private void LogoSplash()
+    {
+        _logoGroup.gameObject.SetActive(true);
+        Sequence seq = DOTween.Sequence();
+        seq.Append(_logoGroup.transform.DORotate(new Vector3(0, 0, 360f), 1f, RotateMode.FastBeyond360));
+        seq.Join(_logoGroup.transform.DOShakeScale(0.8f));
+        seq.Append(_logoGroup.DOFade(0f, 1f));
+        seq.AppendCallback(() =>
+        {
+            _logoGroup.gameObject.SetActive(false);
+            InitUI();
+        });
+    }
+
+    private void InitUI()
+    {
+        _mainPanel.SetActive(true);
+    }
+
+    public void GoStageSelectScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoOption()
+    {
+
+    }
+}
