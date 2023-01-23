042ac7ac77df46dab8e58cbd0de2fc9114a606e3
Taeyoung <l32590838@gmail.com>
2023-01-24 01:00:28 +0900

    스타트 수정

--- Assets/01.Script/0.Core/UI/InteractiveButton.cs
+++ Assets/01.Script/0.Core/UI/InteractiveButton.cs
@@ -4,23 +4,48 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using DG.Tweening;
+using TMPro;
 
 public class InteractiveButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] protected Color originColor = Color.white;
     [SerializeField] protected Color activeColor = Color.white;
     [SerializeField] protected float activeMoveValue = 0;
     [SerializeField] protected float activeSizeValue = 1;
-    [SerializeField] protected AudioClip activeClip;
-    protected Image image;
+    [SerializeField] protected AudioClip hoverClip;
+    [SerializeField] protected AudioClip clickClip;
+
+    [Header("Materials")]
+    [SerializeField] private TextMeshProUGUI[] textList;
+    [SerializeField] private Image[] imageList;
+
     protected RectTransform rectTransform;
-    protected Color originColor;
     protected float originPos;
     public virtual void Awake()
     {
-        image = GetComponent<Image>();
         rectTransform = GetComponent<RectTransform>();
         originPos = rectTransform.anchoredPosition.x;
-        originColor = image.color;
+
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            if (clickClip != null)
+            {
+                AudioManager.PlayAudioRandPitch(clickClip);
+            }
+        });
+
+        Kill();
+
+        rectTransform.DOAnchorPosX(originPos, 0.5f).SetUpdate(true);
+        rectTransform.DOScale(1, 0.25f).SetUpdate(true);
+        foreach (var mat in textList)
+        {
+            mat.DOColor(originColor, 0.4f).SetUpdate(true);
+        }
+        foreach (var mat in imageList)
+        {
+            mat.DOColor(originColor, 0.2f).SetUpdate(true);
+        }
     }
     public virtual void OnPointerEnter(PointerEventData eventData)
     {
@@ -28,10 +53,17 @@ public class InteractiveButton : MonoBehaviour, IPointerEnterHandler, IPointerEx
 
         rectTransform.DOAnchorPosX(originPos + activeMoveValue, 0.25f).SetUpdate(true);
         rectTransform.DOScale(activeSizeValue, 0.2f).SetUpdate(true);
-        image.DOColor(activeColor, 0.2f).SetUpdate(true);
-        if(activeClip != null)
+        foreach (var mat in textList)
         {
-            AudioManager.PlayAudioRandPitch(activeClip);
+            mat.DOColor(activeColor, 0.2f).SetUpdate(true);
+        }
+        foreach (var mat in imageList)
+        {
+            mat.DOColor(activeColor, 0.2f).SetUpdate(true);
+        }
+        if (hoverClip != null)
+        {
+            AudioManager.PlayAudioRandPitch(hoverClip);
         }
     }
 
@@ -41,7 +73,14 @@ public class InteractiveButton : MonoBehaviour, IPointerEnterHandler, IPointerEx
 
         rectTransform.DOAnchorPosX(originPos, 0.5f).SetUpdate(true);
         rectTransform.DOScale(1, 0.25f).SetUpdate(true);
-        image.DOColor(originColor, 0.4f).SetUpdate(true);
+        foreach (var mat in textList)
+        {
+            mat.DOColor(originColor, 0.4f).SetUpdate(true);
+        }
+        foreach (var mat in imageList)
+        {
+            mat.DOColor(originColor, 0.2f).SetUpdate(true);
+        }
     }
 
     private void OnDisable()
@@ -50,12 +89,22 @@ public class InteractiveButton : MonoBehaviour, IPointerEnterHandler, IPointerEx
 
         rectTransform.anchoredPosition = new Vector2(originPos, rectTransform.anchoredPosition.y);
         rectTransform.localScale = Vector3.one;
-        image.color = originColor;
+        foreach (var mat in textList)
+        {
+            mat.color = originColor;
+        }
     }
 
     protected virtual void Kill()
     {
-        image.DOKill();
+        foreach (var mat in textList)
+        {
+            mat.DOKill();
+        }
+        foreach (var mat in imageList)
+        {
+            mat.DOKill();
+        }
         rectTransform.DOKill();
     }
 }
