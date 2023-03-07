9aceef98560b8cd065a2a573b86c2b8d3845b800
Taeyoung <l32590838@gmail.com>
2023-03-07 10:16:01 +0900

    맵배치를 위한 에디터 개발

---
+++ Assets/01.Script/1.Main/Taeyoung/MapEditor/DeployDataBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Core/MapEditorDB", fileName = "MapEditorDB")]
+public class DeployDataBase : ScriptableObject
+{
+
+}
---
+++ Assets/01.Script/1.Main/Taeyoung/MapEditor/Editor/MapElementEditor.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapElement))]
+public class MapElementEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapElement myScript = target as MapElement;
+
+        myScript.deployType = (DeployType)EditorGUILayout.EnumPopup("DeployType", myScript.deployType);
+
+        switch (myScript.deployType)
+        {
+            case DeployType.XY:
+                myScript.size.x = EditorGUILayout.FloatField("Width", myScript.size.x);
+                myScript.size.y = EditorGUILayout.FloatField("Height", myScript.size.y);
+                break;
+            case DeployType.XZ:
+                myScript.size.x = EditorGUILayout.FloatField("Width", myScript.size.x);
+                myScript.size.z = EditorGUILayout.FloatField("Height", myScript.size.z);
+                break;
+            case DeployType.YZ:
+                myScript.size.y = EditorGUILayout.FloatField("Width", myScript.size.y);
+                myScript.size.z = EditorGUILayout.FloatField("Height", myScript.size.z);
+                break;
+        }
+
+        if(GUILayout.Button("Collider 기준으로"))
+        {
+            Collider col = myScript.GetComponent<Collider>();
+            if(col == null)
+            {
+                Debug.LogWarning("콜라이더가 없는데?");
+                return;
+            }
+            switch (myScript.deployType)
+            {
+                case DeployType.XY:
+                    myScript.size.x = col.bounds.size.x;
+                    myScript.size.y = col.bounds.size.y;
+                    break;
+                case DeployType.XZ:
+                    myScript.size.x = col.bounds.size.x;
+                    myScript.size.z = col.bounds.size.z;
+                    break;
+                case DeployType.YZ:
+                    myScript.size.y = col.bounds.size.y;
+                    myScript.size.z = col.bounds.size.z;
+                    break;
+            }
+        }
+    }
+}
