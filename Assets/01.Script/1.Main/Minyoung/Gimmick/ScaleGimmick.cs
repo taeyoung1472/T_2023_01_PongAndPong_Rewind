d45a1267920cc9e8521d646312d083c66d7f994e
hanmin0728 <gksalsdud1234@gmail.com>
2023-03-13 23:26:49 +0900

    저울 기믹 무게 재고 비교기능 만듬

--- Assets/01.Script/1.Main/Minyoung/Gimmick/ScaleGimmick.cs
+++ Assets/01.Script/1.Main/Minyoung/Gimmick/ScaleGimmick.cs
@@ -4,13 +4,86 @@ using UnityEngine;
 
 public class ScaleGimmick : MonoBehaviour
 {
-    void Start()
+    private Collider leftScaleCol;
+    private Collider rightScaleCol;
+    public float rayDistance = 5f;
+
+    public float leftWeight = 0f;
+    public float rightWeight = 0f;
+    private void Start()
     {
-        
+        leftScaleCol = transform.Find("Left").GetComponent<Collider>();
+        rightScaleCol = transform.Find("Right").GetComponent<Collider>();
+    }
+    private void Update()
+    {
+        CheckDown();
+    }
+    private void FixedUpdate()
+    {
+        LeftShootBoxcast();
+        RightShootBoxcast();
+
+    }
+    void LeftShootBoxcast()
+    {
+        Vector3 boxCenter = leftScaleCol.bounds.center;
+        Vector3 halfExtents = leftScaleCol.bounds.extents;
+
+        RaycastHit[] hits = Physics.BoxCastAll(boxCenter, halfExtents, transform.up, transform.rotation, rayDistance);
+        leftWeight = 0;
+
+        foreach (var h in hits)
+        {
+            if (h.transform == null)
+                continue;
+
+            ObjWeight obj = h.transform.GetComponent<ObjWeight>();
+            if (obj == null)
+                continue;
+
+            leftWeight += obj.so.weight;
+        }
+        Debug.Log(leftWeight);
+    }
+    void RightShootBoxcast()
+    {
+        Vector3 boxCenter = rightScaleCol.bounds.center;
+        Vector3 halfExtents = rightScaleCol.bounds.extents;
+
+        RaycastHit[] hits = Physics.BoxCastAll(boxCenter, halfExtents, transform.up, transform.rotation, rayDistance);
+        rightWeight = 0;
+
+        foreach (var h in hits)
+        {
+            if (h.transform == null)
+                continue;
+
+            ObjWeight obj = h.transform.GetComponent<ObjWeight>();
+            if (obj == null)
+                continue;
+
+            rightWeight += obj.so.weight;
+        }
+        Debug.Log(rightWeight);
     }
 
-    void Update()
+    public bool CheckDown()
     {
-        
+        if (leftWeight < rightWeight)
+        {
+            print("오른쪽이무거움");
+            return true;
+        }
+        else
+        {
+            print("왼쪽이무거움");
+            return false;
+        }
+    }
+
+    public void WeightDownPos()
+    {
+        CheckDown();
     }
 }
--- Assets/01.Script/1.Main/Minyoung/SO/ObjWeightSO.cs
+++ Assets/01.Script/1.Main/Minyoung/SO/ObjWeightSO.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjWeightSO : MonoBehaviour
+[CreateAssetMenu(menuName = "SO/Obj/Weight")]
+public class ObjWeightSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float weight;
 }
