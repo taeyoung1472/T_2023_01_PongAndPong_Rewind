fc9f54887baf9ee32dad7d5993280cfed938d247
hanmin0728 <gksalsdud1234@gmail.com>
2023-03-15 20:20:03 +0900

    점프대 구조 다시

--- Assets/01.Script/1.Main/Minyoung/Gimmick/SpringGimmick.cs
+++ Assets/01.Script/1.Main/Minyoung/Gimmick/SpringGimmick.cs
@@ -7,23 +7,37 @@ public class SpringGimmick : MonoBehaviour
 {
     private Collider _col = null;
     public float rayDistance = 1f;
-    private bool isJump = false;
-    private float jumpPower;
+    public bool isJump = false;
+    public int jumpPower;
+
+    [SerializeField]
+    private int _maxDeceleration = 4;
+    public Dictionary<Collider, int> decelerationColDic = new Dictionary<Collider, int>();
+
+    private List<Collider> _cols = new List<Collider>();
     private void Awake()
     {
         _col = GetComponent<Collider>();
     }
     private void Update()
     {
-        //ShootUpBoxCast();
+        ShootUpBoxCast();
     }
-    
-    private void OnTriggerEnter(Collider other)
+
+    private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("hit");
-        if (other.gameObject.CompareTag("Player"))
-            Debug.Log("player connect");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            float time = collision.collider.GetComponent<StayTimeChecker>().StayTime;
+
+            float weight = collision.collider.GetComponent<ObjWeight>().so.weight;
+
+            float ratio = time * weight;
+
+        }
     }
+
+
     //박스캐스트를 쏴서 캐릭터에 부터ㅇ이쓴ㄴ 그 체공시간을 아는걸 같고와야하는거지
     public void ShootUpBoxCast()
     {
@@ -36,26 +50,33 @@ public class SpringGimmick : MonoBehaviour
 
         if (hit.collider != null)
         {
+            if (!decelerationColDic.ContainsKey(hit.collider))
+            {
+                decelerationColDic.Add(hit.collider, 0);
+                decelerationColDic[hit.collider] = _maxDeceleration;
+            }
+            
             float time = hit.collider.GetComponent<StayTimeChecker>().StayTime;
 
             float weight = hit.collider.GetComponent<ObjWeight>().so.weight;
 
             float ratio = time * weight; //라티오가 크면 점프cnt가 크고 라이토가 작으면 점프 cnt가 작아
 
-            jumpPower = 1.5f * ratio;
+            jumpPower = decelerationColDic[hit.collider] * (int)ratio;
+            // 4 2 1 0
 
-            //서서히ㅣ 멈추면 더 
-            //Debug.Log(jumpPower);
+           // Debug.Log(decelerationColDic[hit.collider] + "      " + ratio);
             if (isJump == false)
             {
-                if (jumpPower <= 0)
+                if (decelerationColDic[hit.collider] <= 0)
                 {
                     return;
                 }
-                jumpPower--;
+                decelerationColDic[hit.collider] /= 2;
                 isJump = true;
             }
 
+            Debug.Log(jumpPower);
              hit.collider.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
         }
         else
@@ -63,4 +84,5 @@ public class SpringGimmick : MonoBehaviour
             isJump = false;
         }
     }
+
 }
