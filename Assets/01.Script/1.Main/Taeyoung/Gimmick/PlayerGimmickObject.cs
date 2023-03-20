7f9321a3c1f73c3fca75c662ea3466114d510f98
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2023-03-20 19:10:46 +0900

    0320_바닥 콜라이더 추가, 메인이랑 합체

--- Assets/01.Script/1.Main/Taeyoung/Gimmick/PlayerGimmickObject.cs
+++ Assets/01.Script/1.Main/Taeyoung/Gimmick/PlayerGimmickObject.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class PlayerGimmickObject : GimmickObject
 {
+    private Player _player = null;
+
     public override void AddForce(Vector3 dir, float force, ForceMode forceMode = ForceMode.Impulse)
     {
-
+        _player.GetPlayerAction<PlayerJump>().JumpCountUp();
+        _player.GetPlayerAction<PlayerJump>().ForceJump(dir, force);
     }
 
     public override void Init()
     {
-
+        if (_player == null)
+            _player = GetComponent<Player>();
+        _player.ForceStop();
+        Debug.Log("พร");
     }
 
     public override void RecordTopPosition()
     {
-        
+        if (_player.characterController.velocity.y >= -1f)
+        {
+            recordPosY = transform.position.y;
+        }
     }
 }
--- Assets/01.Script/1.Main/Taeyoung/Gimmick/SpringGimmick.cs
+++ Assets/01.Script/1.Main/Taeyoung/Gimmick/SpringGimmick.cs
@@ -7,7 +7,17 @@ public class SpringGimmick : MonoBehaviour
 {
     public void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.TryGetComponent<GimmickObject>(out GimmickObject obj))
+        ColliderEnter(collision.collider);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ColliderEnter(other);
+    }
+
+    private void ColliderEnter(Collider target)
+    {
+        if (target.transform.root.TryGetComponent<GimmickObject>(out GimmickObject obj))
         {
             float recordPosY = obj.RecordPosY - transform.position.y;
             recordPosY = Mathf.Clamp(recordPosY, 0, 17.5f);
