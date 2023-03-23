6316deec7b08031e0febbd9987d68a902e7b3149
hanmin0728 <gksalsdud1234@gmail.com>
2023-03-23 09:56:16 +0900

    포탈 디졸브 쉐이더 작업중

--- Assets/01.Script/1.Main/Minyoung/Gimmick/Portal/PortalTelepote.cs
+++ Assets/01.Script/1.Main/Minyoung/Gimmick/Portal/PortalTelepote.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using DG.Tweening;
+
 public class PortalTelepote : MonoBehaviour
 {
     public Transform playerTrm;
@@ -10,7 +12,6 @@ public class PortalTelepote : MonoBehaviour
     private Collider _col;
     private bool isPlayerOverlapping;
     public float rayDistance = 1f;
-    private bool isHit;
     public bool isFront;
     public CinemachineVirtualCamera Vcam;
 
@@ -18,9 +19,16 @@ public class PortalTelepote : MonoBehaviour
 
     public bool isRight = true;
     float dotProduct;
+
+
+    public List<Material> playerSkinedMatList = new List<Material>();
+
+
+
     private void Awake()
     {
         _col = GetComponent<Collider>();
+        playerSkinedMatList.AddRange(playerTrm.Find("AgentRenderer/Model/man-mafia_Rig/man-mafia_Rig").GetComponentInChildren<SkinnedMeshRenderer>().materials);
     }
     private void Update()
     {
@@ -33,26 +41,41 @@ public class PortalTelepote : MonoBehaviour
             if (isRight)
             {
                 dotProduct = Vector3.Dot(transform.right * 1f, portalToPlayer);
+
+                foreach (var mat in playerSkinedMatList)
+                {
+                    mat.SetVector("_DissolveDirection", new Vector3(1, 0, 0));
+                }
             }
             else
             {
                 dotProduct = Vector3.Dot(transform.right * -1f, portalToPlayer);
+
+                foreach (var mat in playerSkinedMatList)
+                {
+                    mat.SetVector("_DissolveDirection", new Vector3(-1, 0, 0));
+                }
             }
 
 
             Debug.Log(dotProduct);
-            // If this is true: The player has moved across the portal
             if (dotProduct > 0f)
             {
-                // Teleport him!
+
+                foreach (var mat in playerSkinedMatList)
+                {
+                    mat.SetFloat("_Dissolve", 1f);
+                }
+
                 float rotationDiff = -Quaternion.Angle(transform.rotation, reciverTrm.rotation);
                 rotationDiff += 180;
                 playerTrm.Rotate(Vector3.up, rotationDiff);
 
                 Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
-                Debug.Log(positionOffset);
-                playerTrm.position = reciverTrm.position + positionOffset + Vector3.right * positionOffset.x;
-                //playerTrm.position = new Vector3(playerTrm.position.x, playerTrm.position.y, -1.84f);
+                playerTrm.position = reciverTrm.position + positionOffset + (Vector3.right * (positionOffset.x * 0.5f));
+
+                StartCoroutine(Fade());
+
                 isPlayerOverlapping = false;
             }
         }
@@ -61,26 +84,21 @@ public class PortalTelepote : MonoBehaviour
             playerTrm.GetComponent<CharacterController>().enabled = true;
         }
     }
-  
-    private void FixedUpdate()
+    IEnumerator Fade()
     {
-        RaycastHit hit;
+       
+        yield return new WaitForSeconds(0.5f);
 
-        Vector3 boxCenter = _col.bounds.center;
-        Vector3 halfExtents = _col.bounds.extents;
-
-        isHit = Physics.BoxCast(boxCenter, new Vector3(halfExtents.x, halfExtents.y, halfExtents.z), renderTrm.up, out hit, transform.rotation, rayDistance);
-
-        if (isHit)
+        foreach (var mat in playerSkinedMatList)
         {
-            isFront = true;
-        }
-        else
-        {
-            isFront = false;
+            DoFade(0.5f, 0, 1f, mat);
         }
     }
 
+    public void DoFade(float start, float dest, float time, Material dissolveMat)
+    {
+        DOTween.To(() => start, x => { start = x; dissolveMat.SetFloat("_Dissolve", start); }, dest, time).SetUpdate(true);
+    }
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("잉이이");
@@ -89,12 +107,39 @@ public class PortalTelepote : MonoBehaviour
             Debug.Log("플레이어와 충돌함");
             isPlayerOverlapping = true;
         }
+        if (other.gameObject.layer == LayerMask.NameToLayer("Gimmick"))
+        {
+            TelObj(other.transform);
+        }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("GimmickPlayerCol"))
         {
             isPlayerOverlapping = false;
+        }
+        if (other.gameObject.layer == LayerMask.NameToLayer("Gimmick"))
+        {
+            Debug.Log("물체와 충돌함");
+        }
+    }
+    private void TelObj(Transform colTrm)
+    {
+        Debug.Log("텔오비지");
+        Vector3 portalToObj = colTrm.position - transform.position;
+
+        if (isRight)
+        {
+            dotProduct = Vector3.Dot(transform.right * 1f, portalToObj);
+        }
+        else
+        {
+            dotProduct = Vector3.Dot(transform.right * -1f, portalToObj);
+        }
+
+        if (dotProduct > 0f)
+        {
+            colTrm.position = reciverTrm.position + new Vector3(5, 0, 0);
         }
     }
 }
