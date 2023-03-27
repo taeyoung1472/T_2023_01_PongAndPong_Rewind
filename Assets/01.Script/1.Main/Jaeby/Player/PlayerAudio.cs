e7d148e5a2fd89e58e1460cb72e323a9065a5ca3
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2023-03-27 18:18:00 +0900

    0327_병합 찌꺼기, 발소리 임포트

--- Assets/01.Script/1.Main/Jaeby/Player/PlayerAudio.cs
+++ Assets/01.Script/1.Main/Jaeby/Player/PlayerAudio.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerAudio : MonoBehaviour
 {
     [SerializeField]
-    private AudioClip _moveClip = null;
+    private List<AudioClip> _moveClips = new List<AudioClip>();
 
     public void MoveAudio()
     {
         AudioPoolObject audio = PoolManager.Pop(PoolType.Sound).GetComponent<AudioPoolObject>();
-        audio.Play(_moveClip, Random.Range(0.5f, 1f));
+        audio.Play(_moveClips[Random.Range(0, _moveClips.Count)], Random.Range(0.5f, 1f));
     }
 }
