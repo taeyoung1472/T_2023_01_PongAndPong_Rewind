bd55d23815281b6a910c1e46d68ebca369fd2b98
hanmin0728 <gksalsdud1234@gmail.com>
2023-03-28 23:17:39 +0900

    자석 기믹 완성

--- Assets/01.Script/1.Main/Minyoung/Gimmick/MagnetGimmick.cs
+++ Assets/01.Script/1.Main/Minyoung/Gimmick/MagnetGimmick.cs
@@ -2,15 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagnetGimmick : GimmickObject
+public class MagnetGimmick : MonoBehaviour
 {
-    public override void Init()
-    {
-    }
+    public float speed = 5f;
 
+    private Vector3 lookMagnetVec = Vector3.zero;
+
+    private float radius;
+    private void Awake()
+    {
+        radius = GetComponent<SphereCollider>().radius;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.TryGetComponent<MagnetGimmickObject>(out MagnetGimmickObject magent))
+        {
+        }
+    
     }
-
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.TryGetComponent<MagnetGimmickObject>(out MagnetGimmickObject magent))
+        {
+            Debug.Log(magent);
+            lookMagnetVec = transform.position - other.transform.position;
+            float ratio = (1 - (lookMagnetVec.magnitude / radius)) * speed;
+            Debug.Log(ratio);
+            Rigidbody rid = magent.GetComponent<Rigidbody>();
+            rid.velocity = lookMagnetVec * ratio;
+        }   
+    }
 }
