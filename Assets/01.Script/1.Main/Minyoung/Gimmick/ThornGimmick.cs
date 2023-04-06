7d21ff9f4d926273b355448571e6649d9cb3aec6
hanmin0728 <gksalsdud1234@gmail.com>
2023-04-06 23:31:31 +0900

    가시기믹 발판기믹 마그넷 발판 타이머 기믹들 버그수정 및 안정화

--- Assets/01.Script/1.Main/Minyoung/Gimmick/ThornGimmick.cs
+++ Assets/01.Script/1.Main/Minyoung/Gimmick/ThornGimmick.cs
@@ -2,16 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThornGimmick : MonoBehaviour
+public class ThornGimmick : GimmickObject
 {
-    private void OnTriggerEnter(Collider other)
+    private Collider _col;
+
+    public bool isCheck = false;
+
+    [SerializeField] private float rayDistance = 0.5f;
+
+    RaycastHit hit;
+
+    public bool isDie;
+    private void Awake()
     {
-        if (other.gameObject.CompareTag("GimmickPlayerCol"))
+        Init();
+    }
+    public override void Init()
+    {
+        _col = GetComponent<BoxCollider>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 boxCenter = _col.bounds.center;
+        Vector3 halfExtents = _col.bounds.extents;
+
+        isCheck = Physics.BoxCast(boxCenter, halfExtents, transform.up, out hit, transform.rotation, rayDistance);
+
+        if (isCheck)
         {
-            Player player = other.GetComponentInParent<Player>();
-            Debug.Log(player);
-            //재엽이가만들 플레이어다이에 다이함수
-            //player.GetComponentInChildren<PlayerDie>().Die();
-        }       
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("GimmickPlayer"))
+            {
+                Player player = hit.collider.GetComponentInParent<Player>();
+                if (isDie)
+                    return;
+                player.playerHP.Die();
+                isDie = true;
+            }
+        }
     }
 }
