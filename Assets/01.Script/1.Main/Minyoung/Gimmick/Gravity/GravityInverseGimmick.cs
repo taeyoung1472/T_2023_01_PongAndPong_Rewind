d815e737a32a1353719320959709e415e09655ff
hanmin0728 <gksalsdud1234@gmail.com>
2023-04-09 23:40:14 +0900

    중력 기믹 안정화완료 (플레이어 물체) 상하

--- Assets/01.Script/1.Main/Minyoung/Gimmick/Gravity/GravityInverseGimmick.cs
+++ Assets/01.Script/1.Main/Minyoung/Gimmick/Gravity/GravityInverseGimmick.cs
@@ -13,63 +13,9 @@ public class GravityInverseGimmick : GimmickObject
     {
         Up,
         Down,
-        Left,
-        Right,
     }
 
     public GravityDirState gravityDirState;
-
-
-    private void OnTriggerStay(Collider other)
-    {
-        /*
-        if (other.CompareTag("GimmickPlayerCol"))
-        {
-            player.GravityModule.GravityScale = 0.2f;
-
-            switch (gravityDirState)
-            {
-                case GravityDirState.Up:
-                    module.GravityDir = new Vector3(0f, 9.8f, 0f);
-                    player.transform.rotation = Quaternion.Euler(180f, player.transform.rotation.y, player.transform.rotation.z);
-                    break;
-                case GravityDirState.Down:
-                    module.GravityDir = new Vector3(0f, -9.8f, 0f);
-                    player.transform.rotation = Quaternion.Euler(0f, player.transform.rotation.y, player.transform.rotation.z); 
-                    break;
-                case GravityDirState.Left:
-                    module.GravityDir = new Vector3(-9.8f, 0f, 0f);
-                    player.transform.rotation = Quaternion.Euler(player.transform.rotation.x, player.transform.rotation.y, -90f);
-                    break;
-                case GravityDirState.Right:
-                    module.GravityDir = new Vector3(9.8f, 0f, 0f);
-                    player.transform.rotation = Quaternion.Euler(player.transform.rotation.x, player.transform.rotation.y, 90f);
-                    break;
-            }
-
-        }
-
-        if (other.gameObject.TryGetComponent<GravityGimmickObject>(out GravityGimmickObject gravityGimmick))
-        {
-           // gravityGimmick.GravityScale = 0.5f;
-            switch (gravityDirState)
-            {
-                case GravityDirState.Up:
-                    gravityGimmick.GravityVec = new Vector3(0, 9.8f, 0);
-                    break;
-                case GravityDirState.Down:
-                    gravityGimmick.GravityVec = new Vector3(0, -9.8f, 0);
-                    break;
-                case GravityDirState.Left:
-                    gravityGimmick.GravityVec = new Vector3(-9.8f, 0, 0);
-                    break;
-                case GravityDirState.Right:
-                    gravityGimmick.GravityVec = new Vector3(9.8f, 0, 0);
-                    break;
-            }
-        }
-        */
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -80,15 +26,17 @@ public class GravityInverseGimmick : GimmickObject
             if (player == null)
                 player = other.GetComponent<Player>();
 
-
-            //player.transform.rotation = Quaternion.Euler(180f, player.transform.rotation.eulerAngles.y, 0f);
-
-
             player.PlayerRenderer.flipDirection = FlipDirection.Up;
-            module.GravityScale = 0.4f;
-            Debug.Log(module.GravityScale);
+            //module.GravityScale = 0.4f;
+            module.OriginGravityScale = 0.4f;
 
             module.GravityDir = new Vector3(0f, 9.8f, 0f);
+        }
+        if (other.gameObject.TryGetComponent<GravityGimmickObject>
+            (out GravityGimmickObject gravityGimmick))
+        {
+            gravityGimmick.GravityDir = new Vector3(0, 9.8f, 0f);
+            gravityGimmick.GravityScale = 0.4f;
         }
     }
 
@@ -98,9 +46,17 @@ public class GravityInverseGimmick : GimmickObject
         {
             return;
         }
-        module.GravityScale = module.OriginGravityScale;
         player.PlayerRenderer.flipDirection = FlipDirection.Down;
+        module.OriginGravityScale = 0.8f;
+        //module.GravityScale = module.OriginGravityScale;
 
         module.GravityDir = new Vector3(0f, -9.8f, 0f);
+
+        if (other.gameObject.TryGetComponent<GravityGimmickObject>
+                  (out GravityGimmickObject gravityGimmick))
+        {
+            gravityGimmick.GravityDir = new Vector3(0, -9.8f, 0f);
+            gravityGimmick.GravityScale = gravityGimmick.OrignGravityScale;
+        }
     }
 }
