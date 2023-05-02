9db5da754a6a2d587232089e4fead8d7659b6801
Taeyoung <l32590838@gmail.com>
2023-05-02 09:48:35 +0900

    스테이지 작업

--- Assets/01.Script/1.Main/Taeyoung/Gimmick/ButtonGimmick.cs
+++ Assets/01.Script/1.Main/Taeyoung/Gimmick/ButtonGimmick.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
-using DG.Tweening;
+
 public class ButtonGimmick : GimmickObject
 {
     bool isActive = false;
@@ -171,7 +171,7 @@ public class ButtonGimmick : GimmickObject
                 Debug.Log(player);
             }
         }
-        
+
         if (other.TryGetComponent<GimmickObject>(out GimmickObject gimmickObject))
         {
             Debug.Log(gimmickObject);
