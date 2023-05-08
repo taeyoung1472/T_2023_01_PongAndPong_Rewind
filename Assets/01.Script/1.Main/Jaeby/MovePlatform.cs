145f2098600469a2c2b83ff82506c8bd0de6dfe6
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2023-05-08 18:53:15 +0900

    0508_1-2스테이지 무브 플랫폼만

--- Assets/01.Script/1.Main/Jaeby/MovePlatform.cs
+++ Assets/01.Script/1.Main/Jaeby/MovePlatform.cs
@@ -75,7 +75,7 @@ public class MovePlatform : GimmickObject
             }
             Vector3 newVec = _objList[i].transform.position;
             newVec.y = _parentTrm.position.y +
-                _parentTrm.transform.localScale.y * 0.49f;
+                _parentTrm.GetComponent<BoxCollider>().size.y * 0.49f;
             _objList[i].transform.position = newVec;
         }
 
