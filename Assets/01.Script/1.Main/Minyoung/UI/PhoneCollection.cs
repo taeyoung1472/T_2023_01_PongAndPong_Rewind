61f0428d7343444b9321eec2eb24b7baa508c7aa
hanmin0728 <gksalsdud1234@gmail.com>
2023-06-05 20:07:41 +0900

    폰 UI 작업중

--- Assets/01.Script/1.Main/Minyoung/UI/PhoneCollection.cs
+++ Assets/01.Script/1.Main/Minyoung/UI/PhoneCollection.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.UI;
 public class PhoneCollection : MonoSingleTon<PhoneCollection>
 {
     private List<ChapterStageCollectionData> chapterList = new List<ChapterStageCollectionData>();
@@ -15,15 +15,19 @@ public class PhoneCollection : MonoSingleTon<PhoneCollection>
     [SerializeField] private Transform parentTrm;
     [SerializeField] private GameObject chapterCellPrefab;
 
-    [SerializeField] private Transform chapterParentTrm;
     [SerializeField] private GameObject stageCellPrefab;
+
 
 
     void Start()
     {
         for (int i = 0; i < stageDatabase.worldList.Count; i++)
         {
-            GameObject obj = Instantiate(chapterCellPrefab, parentTrm);
+            GameObject chObj = Instantiate(chapterCellPrefab, parentTrm);
+            for (int j = 0; j < stageDatabase.worldList[i].stageList.Count; j++)
+            {
+                GameObject stObj = Instantiate(stageCellPrefab, chObj.transform);
+            }
         }
     }
     public void OnCollectionMenu()
@@ -47,7 +51,7 @@ public class PhoneCollection : MonoSingleTon<PhoneCollection>
 
         for (int i = 0; i < parentTrm.childCount; i++)
         {
-            childObjs[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText($"{i + 1} √©≈Õ{chapterNameList[i]}");
+           childObjs[i].transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().SetText($"{i + 1} √©≈Õ{chapterNameList[i]}");
 
             foreach (var s in chapterList[i].stageCollectionDataList)
                 maxCnt += s.collectionBoolDataList.Count;
@@ -55,21 +59,10 @@ public class PhoneCollection : MonoSingleTon<PhoneCollection>
             foreach (var e in chapterList[i].stageCollectionDataList)
                 eatCnt += e.collectionBoolDataList.FindAll(x => x == true).Count;
 
-            childObjs[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().SetText($"{eatCnt} / {maxCnt}");
+            childObjs[i].transform.GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>().SetText($"{eatCnt} / {maxCnt}");
 
             maxCnt = 0;
             eatCnt = 0;
         }
     }
-
-    public void OnChapterCollection(int index)
-    {
-        mainText.SetText($"{index + 1} √©≈Õ ¡§∫∏");
-        for (int i = 0; i < chapterList[index].stageCollectionDataList.Count; i++)
-        {
-            GameObject obj = Instantiate(stageCellPrefab, chapterParentTrm);
-        }
-    }
-
-
 }
