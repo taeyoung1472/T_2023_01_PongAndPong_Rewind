2db55b2df2af26556f000de32d441f9958d292a0
Taeyoung <l32590838@gmail.com>
2023-06-29 09:59:28 +0900

    버그 수정

--- Assets/01.Script/1.Main/Jaeby/StageSelectUI/ChapterButtonUI.cs
+++ Assets/01.Script/1.Main/Jaeby/StageSelectUI/ChapterButtonUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChapterButtonUI : MonoBehaviour
 {
@@ -9,10 +10,13 @@ public class ChapterButtonUI : MonoBehaviour
     private TextMeshProUGUI _worldNameText = null;
     [SerializeField]
     private TextMeshProUGUI _chapterNameText = null;
+    [SerializeField]
+    private Image _chapterBackgroundImg;
 
     public void NameSet(StageWorldUI ui)
     {
         _worldNameText.SetText(ui.WorldName);
         _chapterNameText.SetText("ц╘ем " + ui.ChapterName);
+        _chapterBackgroundImg.sprite = ui.chapterBackgroundSprite;
     }
 }
