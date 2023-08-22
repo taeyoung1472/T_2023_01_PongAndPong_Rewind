16028366d756e84b7cb4cd568dd8b499429e3cfd
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2023-08-22 23:36:57 +0900

    0822_힘겨운 프리팹 업데이트

--- Assets/01.Script/1.Main/Jaeby/CutScene/1_1Ending/TrainEndingCutSceneController.cs
+++ Assets/01.Script/1.Main/Jaeby/CutScene/1_1Ending/TrainEndingCutSceneController.cs
@@ -1,8 +1,11 @@
+using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
 
 public class TrainEndingCutSceneController : MonoBehaviour
 {
@@ -12,15 +15,42 @@ public class TrainEndingCutSceneController : MonoBehaviour
     private TrailableObject _trailableObj = null;
     [SerializeField]
     private StageCommunicationSO _comuData = null;
+    [SerializeField]
+    private ParticleSystem _particleSystem = null;
 
+    private void Start()
+    {
+    }
+
+    private void Dynamicbinding()
+    {
+        TimelineAsset ta = _cutScene.playableAsset as TimelineAsset;
+        IEnumerable<TrackAsset> temp = ta.GetOutputTracks();
+        foreach (TrackAsset track in temp)
+        {
+            if(track is CinemachineTrack)
+            {
+                Debug.Log("찾았다.");
+                _cutScene.SetGenericBinding(track, Utility.SearchByClass<CinemachineBrain>());
+            }
+        }
+    }
+
+    [ContextMenu("테스트로 시작")]
     public void CutSceneStart()
     {
+        TimerManager.Instance.EndRewind();
+        Dynamicbinding();
         _cutScene.Play();
     }
 
     public void TrailStart()
     {
-        _trailableObj.TrailEnable();
+        TrailManager.Instance.AddTrailObj(_trailableObj);
+        _trailableObj.IsMotionTrail = true;
+        //_trailableObj.TrailEnable();
+        _particleSystem.gameObject.SetActive(true);
+        _particleSystem.Play();
     }
 
     public void ComuStart()
